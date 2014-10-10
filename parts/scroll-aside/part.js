dcd433c792e0f4a78fbe1b97b4182cfa4c375630
noyesno <noyesno.net@gmail.com>
2014-10-10 04:37:38 -0600

    add sticky to scroll-aside

--- parts/scroll-aside/part.js
+++ parts/scroll-aside/part.js
@@ -4,7 +4,10 @@
 
   var defaults = {
     latency:200,
-    top:36
+    top:36,
+    effect: 'scroll', // sticky
+    sticky:'sticky',
+    callback:null,
   };
 
   function Plugin(element, options){
@@ -23,20 +26,36 @@
 
   Plugin.prototype.init = function(){
     var self=this, $that = this.$that, options=this.options;
+    this.options.offsetTop = $that.offset().top;
     var arg_latency = options.latency,
-        arg_top     = options.top;
+        arg_top     = options.top,
+        effect      = options.effect,
+        offsetTop   = options.offsetTop;
     var $parent = $that.offsetParent();
+
     if($that.attr('data-top')){
       options_top = parseInt($that.attr('data-top'), 10);
     }
     
     function onscroll(){
       var doc_top = $(this).scrollTop();
-      var top = doc_top + arg_top - $parent.offset().top;
-      $that.animate({'top': [top, 'px'].join('')});
+
+      if(effect=="scroll"){
+        var top = doc_top + arg_top - $parent.offset().top;
+        $that.animate({'top': [top, 'px'].join('')});
+        return;
+      }
+      if(effect=="sticky"){
+        if(doc_top > (offsetTop + arg_top)){
+          $that.addClass(options.sticky);
+        }else{
+          $that.removeClass(options.sticky);
+        }
+      }
     }
     
-    $(document).scroll(debounce(arg_latency, onscroll));
+    var action = options.callback?options.callback:onscroll; 
+    $(document).scroll(debounce(arg_latency, action));
   };
   
 
